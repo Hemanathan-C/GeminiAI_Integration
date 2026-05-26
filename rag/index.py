9d2ba997ccb1db5b17a121492aa28a295362f70e
Hemanathan C <hemanathanc07@gmail.com>
2026-05-26 07:29:09 +0530

    Data chunk and vector embeddings dump

--- rag/index.py
+++ rag/index.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
+
 from pathlib import path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
 
 pdf_path = Path(__file__).parent / "nodejs.pdf"
 
@@ -7,3 +12,23 @@ pdf_path = Path(__file__).parent / "nodejs.pdf"
 
 loader = PyPDFLoader(file_path=pdf_path)
 docs = loader.load()
+
+#Split the Docs into chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 400
+)
+
+chunks = text_splitter.split_documents(documents=docs)
+
+#Vecotr Embeddings
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents = chunks,
+    embedding = embedding_model,
+    url = "http://localhost:6333", #Qdrant url
+    collection_name = "vector_embedding"
+)
